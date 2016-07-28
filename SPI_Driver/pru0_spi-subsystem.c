1217846ea31d9a37268ffa63e3fddec80da9edb9
chanakya-vc <vaibhav.sanskriti@gmail.com>
2016-07-29 05:04:35 +0530

    Fixed compilation errors in spi driver

--- SPI_Driver/pru0_spi-subsystem.c
+++ SPI_Driver/pru0_spi-subsystem.c
@@ -8,131 +8,133 @@
  *1) Redistributions/adaptions of source code must retain this copyright
  *   notice on the top, giving credit to the original author, along with 
  *   this list of conditions.
- *
- *2) Redistributions in binary form, compiled from this source code and/or 
- *   modified/adapted versions of this source code, must include this copyright 
- *   notice giving credit to the original author, along with this list of conditions 
- *   in the documentation and other materials provided with the
- *   distribution.
- *
- *3) The original author shall not held for any loss arising from using this code.
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE
- */
-# include <linux/kernel.h>
-# include <linux/module.h>
-# include <asm/io.h> //for ioremap
-# include <linux/ioport.h>
-# include <linux/slab.h> //for allocating memory
-# include <linux/spi/spi.h>//defines all the structures used by the spi-subsytem
-# include <linux/platform_device.h> //Defines structure for using the platform-bus
-# include <linux/err.h>
-#define DRIVER_NAME "pru0_spi_vc"
-
-struct pru0_spi={
-struct spi_master *master;
-
-static void * Data_pointer_mosi; 
-static void * Data_pointer_miso; 
-static void * flag_mosi;
-static void * flag_miso;  
-static void *spi_cs;
-static void *spi_lsb_first;
-static void *spi_cpol;
-static void *spi_cpha;
-};
-static int pru0_spi_setup(struct spi_device *spi)
-{
-	uint8_t spi_cs_val;
-	uint8_t spi_lsb_first_val;
-	uint8_t spi_cpol_val;
-	uint8_t spi_cpha_val;
-	if(!(spi->mode & SPI_CS_HIGH))
-	{
-		spi_cs_val=0;
-		iowrite8(spi_cs_val,pru0->spi_cs); //normal cs ,i.e low on active
-	}
-	else
-	{
-		spi_cs_val=0x1;
-		iowrite8(spi_cs_val,pru0->spi_cs);
-	}
-
-	if(spi_>mode & SPI_LSB_FIRST)
-	{
-		spi_lsb_first_val=1;
-		iowrite8(spi_lsb_first_val,pru0->spi_lsb_first); //lsb first tranfer will take place
-	}
-	else
-	{
-		spi_lsb_first_val=0;
-		iowrite8(spi_lsb_first_val,pru0->spi_lsb_first); //msb first tranfer will take place
-	}
-	if(spi->mode & SPI_CPOL)
-	{
-		spi_cpol_val=0x1;
-		iowrite8(spi_cpol_val,pru0->spi_cpol);
-	}
-	else
-	{
-		spi_cpol_val=0;
-		iowrite8(spi_cpol_val,pru0->spi_cpol);	
-	}
-	if(spi->mode & SPI_CPHA)
-	{
-		spi_cpha_val=0x1;
-		iowrite8(spi_cpha_val,pru0->spi_cpha);
-	}
-	else
-	{
-		spi_cpha_val=0;
-		iowrite8(spi_cpha_val,pru0->spi_cpha);	
-	}
-
-}	
-static int pru0_spi_transfer_one(struct spi_master *master,struct spi_device *spi, struct spi_transfer *t)
-{
-    const void      *tx_buf = t->tx_buf ;        
-	void            *rx_buf = t->rx_buf;
-	pru0=spi_master_get_devdata(master);
-	uint8_t mosi_transfer=*tx_buf;
-	uint8_t mosi_flag_val=0x1;
-	static void *mosi=pru0 -> Data_pointer_mosi;
-	static void *miso=pru0 -> Data_pointer_miso;
-	static void *mosi_flag=pru0 -> flag_mosi;
-	if(tx_buf!=NULL)
-	{
-		iowrite8(mosi_transfer,mosi);
-		iowrite8(mosi_flag_val,mosi_flag); //set value for the flag to 1 
-	}
-	if(miso!=NULL)
-	{
-		*rx_buf=ioread8(miso);
-	}
-}
-
-static int pru0_spi_remove(struct platform_device *pdev)
-{
-	struct spi_master *master= 	platform_get_drvdata(pdev);
-	struct pru0_spi=spi_master_get_devdata(master);
-	release_mem_region(0x4a310000, 56);
-	iounmap(pru0->Data_pointer_mosi);
-	iounmap(pru0->Data_pointer_miso);
-	iounmap(pru0->flag_mosi);
-	iounmap(pru0->flag_miso);
-	iounmap(pru0->spi_cs);
-	iounmap(pru0->spi_lsb_first);
-	iounmap(pru0->spi_cpol);
-	iounmap(pru0->spi_cpha);
-	pru0->Data_pointer_mosi=NULL;
-	pru0->Data_pointer_miso=NULL;
-	pru0->flag_mosi=NULL;
-	pru0->flag_miso=NULL;
-	pru0->spi_cs=NULL;
-	pru0->spi_lsb_first=NULL;
-	pru0->spi_cpol=NULL;
-	pru0->spi_cpha=NULL;
+ *(
+  *2) Redistributions in binary form, compiled from this source code and/or 
+  *   modified/adapted versions of this source code, must include this copyright 
+  *   notice giving credit to the original author, along with this list of conditions 
+  *   in the documentation and other materials provided with the
+  *   distribution.
+  *
+  *3) The original author shall not held for any loss arising from using this code.
+  *   This program is distributed in the hope that it will be useful,
+  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE
+  */
+ # include <linux/kernel.h>
+ # include <linux/module.h>
+ # include <asm/io.h> //for ioremap
+ # include <linux/ioport.h>
+ # include <linux/slab.h> //for allocating memory
+ # include <linux/spi/spi.h>//defines all the structures used by the spi-subsytem
+ # include <linux/platform_device.h> //Defines structure for using the platform-bus
+ # include <linux/err.h>
+ #define DRIVER_NAME "pru0_spi_vc"
+ 
+ struct pru0_spi {
+ struct spi_master *master;
+ void *Data_pointer_mosi; 
+ void *Data_pointer_miso; 
+ void *flag_mosi;
+ void *flag_miso;  
+ void *spi_cs;
+ void *spi_lsb_first;
+ void *spi_cpol;
+ void *spi_cpha;
+ };
+ static int pru0_spi_setup(struct spi_device *spi)
+ {
+ 	struct pru0_spi   *pru0=spi_master_get_devdata(spi->master);
+ 	uint8_t spi_cs_val;
+ 	uint8_t spi_lsb_first_val;
+ 	uint8_t spi_cpol_val;
+ 	uint8_t spi_cpha_val;
+ 	if(!(spi->mode & SPI_CS_HIGH))
+ 	{
+ 		spi_cs_val=0;
+ 		iowrite8(spi_cs_val,pru0->spi_cs); //normal cs ,i.e low on active
+ 	}
+ 	else
+ 	{
+ 		spi_cs_val=0x1;
+ 		iowrite8(spi_cs_val,pru0->spi_cs);
+ 	}
+ 
+ 	if(spi->mode & SPI_LSB_FIRST)
+ 	{
+ 		spi_lsb_first_val=1;
+ 		iowrite8(spi_lsb_first_val,pru0->spi_lsb_first); //lsb first tranfer will take place
+ 	}
+ 	else
+ 	{
+ 		spi_lsb_first_val=0;
+ 		iowrite8(spi_lsb_first_val,pru0->spi_lsb_first); //msb first tranfer will take place
+ 	}
+ 	if(spi->mode & SPI_CPOL)
+ 	{
+ 		spi_cpol_val=0x1;
+ 		iowrite8(spi_cpol_val,pru0->spi_cpol);
+ 	}
+ 	else
+ 	{
+ 		spi_cpol_val=0;
+ 		iowrite8(spi_cpol_val,pru0->spi_cpol);	
+ 	}
+ 	if(spi->mode & SPI_CPHA)
+ 	{
+ 		spi_cpha_val=0x1;
+ 		iowrite8(spi_cpha_val,pru0->spi_cpha);
+ 	}
+ 	else
+ 	{
+ 		spi_cpha_val=0;
+ 		iowrite8(spi_cpha_val,pru0->spi_cpha);	
+ 	}
+ 	return 0 ;
+ 
+ }	
+ static int pru0_spi_transfer_one(struct spi_master *master,struct spi_device *spi, struct spi_transfer *t)
+ {
+     const uint8_t     *tx_buf = t->tx_buf ;        
+           uint8_t     *rx_buf = t->rx_buf;
+ 	struct pru0_spi *pru0=spi_master_get_devdata(master);
+ 	uint8_t mosi_transfer=*tx_buf;
+ 	uint8_t mosi_flag_val=0x1;
+ 	void *mosi=pru0 -> Data_pointer_mosi;
+    void *miso=pru0 -> Data_pointer_miso;
+    void *mosi_flag=pru0 -> flag_mosi;
+ 	if(tx_buf!=NULL)
+ 	{
+ 		iowrite8(mosi_transfer,mosi);
+ 		iowrite8(mosi_flag_val,mosi_flag); //set value for the flag to 1 
+ 	}
+ 	if(miso!=NULL)
+ 	{
+ 		*rx_buf=ioread8(miso);
+ 	}
+ return 0;
+ }
+ 
+ static int pru0_spi_remove(struct platform_device *pdev)
+ {
+ 	struct spi_master *master= 	platform_get_drvdata(pdev);
+ 	struct pru0_spi   *pru0=spi_master_get_devdata(master);
+ 	release_mem_region(0x4a310000, 56);
+ 	iounmap(pru0->Data_pointer_mosi);
+ 	iounmap(pru0->Data_pointer_miso);
+ 	iounmap(pru0->flag_mosi);
+ 	iounmap(pru0->flag_miso);
+ 	iounmap(pru0->spi_cs);
+ 	iounmap(pru0->spi_lsb_first);
+ 	iounmap(pru0->spi_cpol);
+ 	iounmap(pru0->spi_cpha);
+ 	pru0->Data_pointer_mosi=NULL;
+ 	pru0->Data_pointer_miso=NULL;
+ 	pru0->flag_mosi=NULL;
+ 	pru0->flag_miso=NULL;
+ 	pru0->spi_cs=NULL;
+ 	pru0->spi_lsb_first=NULL;
+ 	pru0->spi_cpol=NULL;
+ 	pru0->spi_cpha=NULL;
 
 }
 static int pru0_spi_probe(struct platform_device *pdev)
@@ -175,7 +177,7 @@ static struct platform_driver pru0_spi_driver={
  };
 module_platform_driver(pru0_spi_driver); //Macro to register the Driver With the Platform bus
 
-MODULE_LICENSE("GPL V2");
+MODULE_LICENSE("GPL");
 MODULE_AUTHOR("VAIBHAV CHOUDHARY");
 MODULE_DESCRIPTION("Driver for Bitbanging SPI on the PRU");	
 
