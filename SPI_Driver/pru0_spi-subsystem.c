daf59f7a78e3ae19ce582330e3bc1cce0de602ff
chanakya-vc <vaibhav.sanskriti@gmail.com>
2016-07-30 02:56:24 +0530

    Removed compiling error

--- SPI_Driver/pru0_spi-subsystem.c
+++ SPI_Driver/pru0_spi-subsystem.c
@@ -96,6 +96,7 @@ static int pru0_spi_transfer_one(struct spi_master *master,
 	void *mosi = pru0->Data_pointer_mosi;
 	void *miso = pru0->Data_pointer_miso;
 	void *mosi_flag = pru0->flag_mosi;
+	void *miso_flag = pru0->flag_miso;
 
 	if (tx_buf != NULL) {
 		iowrite8(mosi_transfer, mosi);
